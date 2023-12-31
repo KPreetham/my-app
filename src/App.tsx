72c45fc08a9481c498712e22f137dea2a6a2858d
Aditya-Maindan <aditya.ci21@sahyadri.edu.in>
2023-12-31 12:17:37 +0530

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+         Hello React JS
         </p>
         <a
           className="App-link"
@@ -16,11 +16,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Create GitHub Account here
         </a>
       </header>
     </div>
-  );
+  );ß
 }
 
 export default App;
